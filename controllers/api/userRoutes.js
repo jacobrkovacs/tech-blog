1844f3e580d98c858804d241fcb51793fd84a915
Jacob Kovacs <jwkovacs.19@gmail.com>
2023-10-11 14:56:11 -0700

    adds profile page

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -82,5 +82,15 @@ router.get('/profile', async (req, res) => {
     }
 });
 
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+    res.render('login')
+  });
 
 module.exports = router;
